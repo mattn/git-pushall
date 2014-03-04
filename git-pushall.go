6a76b1db24873e3eae7a522e5cc58be157c2ebfd
mattn <mattn.jp@gmail.com>
2014-03-04 14:43:45 +0900

    Fix

--- git-pushall.go
+++ git-pushall.go
@@ -7,12 +7,24 @@ import (
 )
 
 func main() {
-	args := append([]string{"push"}, os.Args[1:]...)
+	args := append([]string{"push", "--all"}, os.Args[1:]...)
 	cmd := exec.Command("git", args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
+	if err != nil {
+		if status, ok := cmd.ProcessState.Sys().(syscall.WaitStatus); ok {
+			os.Exit(status.ExitStatus())
+		}
+		os.Exit(1)
+	}
+	args = append([]string{"push", "--tags"}, os.Args[1:]...)
+	cmd = exec.Command("git", args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
 	if err != nil {
 		if status, ok := cmd.ProcessState.Sys().(syscall.WaitStatus); ok {
 			os.Exit(status.ExitStatus())
